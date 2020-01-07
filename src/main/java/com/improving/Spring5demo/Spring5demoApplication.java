aaec270eb88a6bd7f3a6e366705335ae3044f72f
Zach Marbach <Zach.Marbach@improving.com>
2020-01-07 11:00:44 -0600

    first commit

---
+++ src/main/java/com/improving/Spring5demo/Spring5demoApplication.java
@@ -0,0 +1,13 @@
+package com.improving.Spring5demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5demoApplication.class, args);
+	}
+
+}
